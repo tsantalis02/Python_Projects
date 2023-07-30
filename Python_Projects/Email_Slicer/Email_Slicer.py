ce6dc0b049c526c0192423825b03461b3221dbaa
Stergios Tsantalis <103073986+tsantalis02@users.noreply.github.com>
2023-07-30 17:20:28 +0300

    Email_Slicer

---
+++ Python_Projects/Email_Slicer/Email_Slicer.py
@@ -0,0 +1,7 @@
+email = input("Enter your Email: ").strip()
+
+username = email[:email.index('@')]
+domain = email[email.index('@')+1:]
+
+print(f"Your username is {username} & domain is {domain}")
+
