254116fc982b55d99dec878b366925ce3583f4f6
Stergios Tsantalis <103073986+tsantalis02@users.noreply.github.com>
2023-07-31 16:46:34 +0300

    Queue

---
+++ Python_Projects/Queue/queue.py
@@ -0,0 +1,19 @@
+# Initializing a queue
+q = []
+  
+# Adding elements to a queue
+q.append('a')
+q.append('b')
+q.append('c')
+  
+print("Initial queue")
+print(q)
+  
+# Removing elements from a queue
+print("\nElements dequeued from the queue")
+print(q.pop(0))
+print(q.pop(0))
+print(q.pop(0))
+  
+print("\nQueue after removing elements")
+print(q)
